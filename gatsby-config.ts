bf422f19711f03781508bc2af3df4901658d4ec8
lhohw <lho.hw.an1@gmail.com>
2022-12-17 22:11:47 +0900

    remove unused file path

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -51,26 +51,10 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-source-filesystem",
       options: {
-        name: "blog",
-        path: `${__dirname}/contents/blog`,
-      },
-      __key: "blog",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
         name: "setting",
         path: `${__dirname}/contents/setting`,
       },
       __key: "setting",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "slider",
-        path: `${__dirname}/contents/slider`,
-      },
-      __key: "slider",
     },
     {
       resolve: "gatsby-source-filesystem",
