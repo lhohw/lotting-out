a5155bb416d3b204cb1a5bbfc0c494c0d4e90388
lhohw <lho.hw.an1@gmail.com>
2022-12-30 17:52:35 +0900

    404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,76 @@
-import * as React from "react"
-import { Link, HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import React from "react";
+import { css } from "@emotion/react";
+import { Link, HeadFC, PageProps } from "gatsby";
+import Layout from "../components/Layout";
+import { useColors } from "../recoil/theme";
 
 const NotFoundPage: React.FC<PageProps> = () => {
+  const colors = useColors();
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Layout>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        `}
+      >
+        <div
+          css={css`
+            padding: 2rem;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border: 2px solid ${colors.main};
+            border-radius: 8px;
+          `}
+        >
+          <h1
+            css={css`
+              color: ${colors.sub};
+              padding: 0;
+              margin: 0;
+              margin-bottom: 2rem;
+            `}
+          >
+            Page Not Found
+          </h1>
+          <p
+            css={css`
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              font-size: 1.05rem;
+              margin-bottom: 2rem;
+              & > span {
+                margin-bottom: 1rem;
+              }
+            `}
+          >
+            <span>관련된 페이지를 찾을 수 없습니다 😔</span>
+            <span>메인 화면으로 돌아가시려면 아래 버튼을 눌러주세요</span>
+          </p>
+          <Link
+            to="/"
+            css={css`
+              font-size: 1.3rem;
+              font-weight: 600;
+              transition: color 0.25s ease-in-out;
+              &:hover {
+                color: ${colors.gold};
+              }
+            `}
+          >
+            🏠 Go home
+          </Link>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Not found</title>;
