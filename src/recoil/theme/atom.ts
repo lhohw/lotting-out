a5d64f3c145063d1803f5066346e0906dc698e11
lhohw <lho.hw.an1@gmail.com>
2023-02-09 21:05:51 +0900

    chore: restore after test

--- src/recoil/theme/atom.ts
+++ src/recoil/theme/atom.ts
@@ -2,13 +2,7 @@ import { atom } from "recoil";
 
 export type ThemeState = "default" | "light" | "dark";
 
-// const initialState = "default" as ThemeState;
-const initialState =
-  typeof window === "undefined"
-    ? "default"
-    : window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : ("light" as ThemeState);
+const initialState = "default" as ThemeState;
 
 const themeState = atom({
   key: "themeState",
